f17907f831d3b9fa230951b13efc3ce898e98cfd
Jari Aalto <jari.aalto@cante.net>
2016-06-12 10:06:41 +0300

    configure-before.sh: autoreconf

--- configure-before.sh
+++ configure-before.sh
@@ -8,7 +8,8 @@ PATH="/sbin:/usr/sbin/:/bin:/usr/bin"
 LC_ALL="C"
 
 # If sources are old, before ./configure, you may need to call:
-# autoreconf --install --force --verbose
+autoreconf --install --force --verbose
+
 #
 # Or this is more lightweight. Probably "autoreconf" is safer choice.
 # libtoolize --force --copy --install
